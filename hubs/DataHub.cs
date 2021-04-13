c704c0a8c515fe52cf8a5df23496e823432800bc
sergey.lvov <sergey.lvov70@gmail.com>
2021-04-13 16:56:49 +0300

    hub and test

---
+++ hubs/DataHub.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using Microsoft.AspNetCore.SignalR.Protocol;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace aspcorer.hubs
+{
+	public class DataHub : Hub
+	{
+		public Task SendData(string user, object data)
+		{
+			return Clients.All.SendAsync("Data", user, data);
+		}
+
+		public async Task Welcome()
+		{
+			await Clients.All.SendAsync("welcome", new[] { new HubMessage(), new HubMessage(), new HubMessage() });
+		}
+
+		public override async Task OnConnectedAsync()
+		{
+			await Welcome();
+			await base.OnConnectedAsync();
+
+		}
+	}
+}
