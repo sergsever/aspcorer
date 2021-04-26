b7f8a2eba5465c23c0f98d2dc5211d857933494c
Sergey Lvov <sergey.lvov70@gmail.com>
2021-04-26 15:30:43 +0300

    hub connected

--- rapp/signal/src/App.tsx
+++ rapp/signal/src/App.tsx
@@ -1,13 +1,26 @@
 import React from 'react';
 import logo from './logo.svg';
+import {HubConnection, HubConnectionBuilder } from '@microsoft/signalr'
 //import './App.css';
 
 class Signal extends React.Component
 {
+  constructor(props: any)
+  {
+    console.log('signal constructor');
+    super(props);
+  }
+  componentDidMount()
+  {
+    console.log('create connection');
+    const connection = new HubConnectionBuilder().withUrl('/datahub').build();
+    connection.start().then(()=> console.log('hub connection:' + connection.state)).catch(err => console.log(err));
+    console.log('connection state: ' + connection.state);
+  }
   render()
   {
     console.log('react render');
-    return <h1>Signal</h1>;
+    return <div><h1>Signal</h1><h3>HubConnect</h3></div>;
   }
 }
 
